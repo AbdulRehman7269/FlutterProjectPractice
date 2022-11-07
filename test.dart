9fd73ffc3471a925a692fcda44e9a7aff26a2e70
AbdulRehman7269 <syedabdulrehman7269@gmail.com>
2022-11-07 18:11:52 +0500

    message

---
+++ test.dart
@@ -0,0 +1,10 @@
+addNumber(double num1, double num2) {
+//print(num1 + num2);
+  return num1 - num2;
+}
+
+void main() {
+  print(addNumber(5, 2.8));
+  print(addNumber(1, 2));
+  print('Hello World');
+}
