92bedc044fd76412e86558b76db7e2e8dac2fda7
AbdulRehman7269 <syedabdulrehman7269@gmail.com>
2022-11-08 11:30:52 +0500

    a

---
+++ 11.2022/8.dart
@@ -0,0 +1,17 @@
+class Person {
+  String name = 'Flutter';
+  int age = 20;
+}
+
+addNumber(num1, num2) {
+  return num1 + num2;
+}
+
+void main() {
+  var p1 = Person();
+  print(p1.name);
+  var firstResult;
+  firstResult = addNumber(5, 2);
+  print(firstResult);
+  print('Hello World!');
+}
