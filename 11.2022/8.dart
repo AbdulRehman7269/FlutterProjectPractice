b025229a92820d37ecc5c563d3c95e26fb693c72
AbdulRehman7269 <syedabdulrehman7269@gmail.com>
2022-11-08 19:57:40 +0500

    a

--- 11.2022/8.dart
+++ 11.2022/8.dart
@@ -14,4 +14,16 @@ void main() {
   firstResult = addNumber(5, 2);
   print(firstResult);
   print('Hello World!');
- }
+}
+--------------------------------------------------------------------------------
+
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+
+  Widget build(BuildContext context) {
+    return MaterialApp(home: (Text('Bulla')),);
+  }
+}
