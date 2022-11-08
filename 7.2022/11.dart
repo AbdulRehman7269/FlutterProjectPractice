0d3d654ee6082e1e57b66655342fad9d91d0992d
AbdulRehman7269 <syedabdulrehman7269@gmail.com>
2022-11-08 11:19:15 +0500

    add:

--- 7.2022/11.dart
+++ 7.2022/11.dart
@@ -1,10 +1,71 @@
-addNumber(double num1, double num2) {
-//print(num1 + num2);
-  return num1 - num2;
+/*double addNumbers(double num1 , double num2) {
+ // print(num1 + num2);
+   return  num1 + num2;
 }
-
 void main() {
-  print(addNumber(5, 2.8));
-  print(addNumber(1, 2));
+ print(addNumbers(1,2.5));
+  print(addNumbers(2+4,2-30));
   print('Hello World');
 }
+
+
+
+
+
+
+
+ addNumber(double num1 ,double num2){
+//print(num1 + num2);
+  return num1 + num2;
+}
+void main()
+{
+  var firstResult;
+  firstResult = addNumber(4,2);
+  print(addNumber(5,2.8));
+  print(addNumber(1,2));
+  print('Hello World');
+  print (firstResult);
+
+}
+*/
+class Person{
+  String name = 'Max';
+  int age = 19;
+}
+addNumber(double num1 ,double num2){
+  return num1 + num2;
+
+}
+void main()
+{
+  var person1 = Person();
+  print(person1.age);
+
+  /*double*/var firstResult;
+  firstResult = addNumber(2,4);
+  print (firstResult);
+  print ('Hello World!');
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
