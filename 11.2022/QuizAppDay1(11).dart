73a5b5e5dfc741b2f5a93cfe946625d90443edaf
AbdulRehman7269 <syedabdulrehman7269@gmail.com>
2022-11-11 19:54:00 +0500

    addchanges

--- 11.2022/QuizAppDay1(11).dart
+++ 11.2022/QuizAppDay1(11).dart
@@ -1,27 +1,34 @@
 import 'package:flutter/material.dart';
-void main() =>  runApp(MyApp());
 
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(
-        title: Text('WhatsApp'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Quiz App'),
+        ),
+        body: Column(
+          children: [
+            Text("What is the colour of Pakistan Flag?"),
+            ElevatedButton(
+              onPressed: () => print("AnswerChoseBlack"),
+              child: Text('Black'),
+            ),
+            ElevatedButton(
+              onPressed: () => print("AnswerChoseGreen"),
+              child: Text('Green'),
+            ),
+            ElevatedButton(
+              onPressed: () => print("AnswerChoseBlue"),
+              child: Text('Blue'),
+            ),
+          ],
+        ),
       ),
-      body: Column(
-        children: [
-          Text('Questions'),
-          ElevatedButton(onPressed: () => print('Answer1 Chosen'), child: Text('Answer1'),
-          ),
-          ElevatedButton(onPressed: () => print('Answer2 Chosen'), child: Text('Answer2'),
-          ),
-          ElevatedButton(onPressed: () => print('Answer3 Chosen'), child: Text('Answer3'),
-          ),
-        ],
-      ),
-    ),
     );
   }
 }
